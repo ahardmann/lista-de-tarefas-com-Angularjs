1e7d0cbd6b31bb27e1cd0a66f4a6f63bab8522ff
unknown <arthur.hage@C305-ads-036.fieb.org.br>
2015-05-12 13:06:37 -0300

    remover tarefa e alert

--- app/ListaTarefasController.js
+++ app/ListaTarefasController.js
@@ -1,13 +1,21 @@
 var myApp = angular.module('myApp',[]);
 
 myApp.controller('ListaTarefasController', ['$scope', function($scope){
-    
+	var task = document.getElementById('newTask');
 	$scope.tarefas = [ ];
 
     $scope.adicionaTarefa = function (){
-    	$scope.tarefas.push({compromisso: $scope.tarefa.compromisso,
-	            concluido: false});
-	    //limpa campo do input
-	    $scope.tarefa.compromisso = '';
+    	if(task.value != ''){
+		    $scope.tarefas.push({compromisso: $scope.tarefa.compromisso,
+			    concluido: false});
+			//limpa campo do input
+			$scope.tarefa.compromisso = '';
+    	}else{
+    		alert("Não foi possivel inserir tarefa!");
+    	}
+    };
+
+    $scope.delete = function(index){
+    	$scope.tarefas.splice(index,1); 
     };
 }]);
